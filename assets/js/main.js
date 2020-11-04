099b8497d4ce0969be82c45c0f79b6ea452e636b
Facundo Aquino <facun90@live.com>
2020-11-03 21:51:17 -0300

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,32 @@ $submit.addEventListener('click',(e)=>{
     $submit.setAttribute('disabled','true')
 
 })
+
+
+/*---------------------- formulario 1 ---------------------*/
+
+
+const $nombre1 = document.getElementById('name1')
+const $tel1 = document.getElementById('tel1')
+const $submit1 = document.getElementById('submit1')
+//por defecto boton desabilitado 
+$submit1.setAttribute('disabled','true')
+ 
+$nombre1.addEventListener('keyup',(e)=>{
+    
+     //expresion regular \d indica todo lo que   es numeros
+     
+    $nombre1.value=$nombre1.value.replace(/\d/,'')
+
+     $nombre1.value&&$tel1.value&&$submit1.removeAttribute('disabled')
+     !$nombre1.vale||!$tel1.value &&  $submit1.setAttribute('disabled','true')
+})
+
+$tel1.addEventListener('keyup',(e)=>{
+     //expresion regular \D indica todo lo que no es numeros
+    $tel1.value=$tel1.value.replace(/\D/,'')
+
+     
+     $nombre1.value&&$tel1.value&&$submit1.removeAttribute('disabled')
+     !$nombre1.value||!$tel1.value &&  $submit1.setAttribute('disabled','true')
+})
